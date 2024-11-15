5dc350374098b71720b2db1dcbd6423c6bbdd96e
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2024-11-15 11:01:53 -0300

    feat: initial config

---
+++ src/main/java/com/portal_egressos/portal_egressos_backend/PortalEgressosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.portal_egressos.portal_egressos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalEgressosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalEgressosBackendApplication.class, args);
+	}
+
+}
