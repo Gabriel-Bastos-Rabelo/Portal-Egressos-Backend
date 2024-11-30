a1ee6ad5f9f6f4a427ae953e71609d177b3ceb0d
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2024-11-30 16:23:43 -0300

    feat: add oportunidade model

--- src/main/java/com/portal_egressos/portal_egressos_backend/models/Oportunidade.java
+++ src/main/java/com/portal_egressos/portal_egressos_backend/models/Oportunidade.java
@@ -1,1 +1,55 @@
 package com.portal_egressos.portal_egressos_backend.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "oportunidade", schema = "public")
+public class Oportunidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_oportunidade")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_egresso", referencedColumnName = "id_egresso", nullable = false)
+    private Egresso egresso;
+
+    @ManyToOne
+    @JoinColumn(name = "id_coordenador", referencedColumnName = "id_coordenador")
+    private Coordenador coordenador;
+
+    @Column(name = "titulo", nullable = false)
+    private String titulo;
+
+    @Column(name = "descricao", nullable = false)
+    private String descricao;
+
+    @Column(name = "local", nullable = false)
+    private String local;
+
+    @Column(name = "tipo", nullable = false)
+    private String tipo;
+
+    @Column(name = "data_publicacao", nullable = false)
+    private LocalDate dataPublicacao;
+
+    @Column(name = "data_expiracao")
+    private LocalDate dataExpiracao;
+
+    @Column(name = "salario")
+    private String salario;
+
+    @Column(name = "link")
+    private String link;
+
+    @Column(name = "status", nullable = false)
+    private String status;
+}
