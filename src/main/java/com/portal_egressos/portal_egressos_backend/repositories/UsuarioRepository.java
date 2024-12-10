53783c9f95be50e38dbab54048e5fa16cb471741
anddersonls <andersonlsilva03@gmail.com>
2024-12-10 11:08:33 -0300

    add: user entity tests

--- src/main/java/com/portal_egressos/portal_egressos_backend/repositories/UsuarioRepository.java
+++ src/main/java/com/portal_egressos/portal_egressos_backend/repositories/UsuarioRepository.java
@@ -1,11 +1,14 @@
 package com.portal_egressos.portal_egressos_backend.repositories;
+import com.portal_egressos.portal_egressos_backend.models.Usuario;
+
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.portal_egressos.portal_egressos_backend.models.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    Optional<Usuario> findByEmail(String email);
+    
 }
--- src/test/java/com/portal_egressos/portal_egressos_backend/repositories/UsuarioRepositoryTest.java
+++ src/test/java/com/portal_egressos/portal_egressos_backend/repositories/UsuarioRepositoryTest.java
@@ -1,4 +1,9 @@
 package com.portal_egressos.portal_egressos_backend.repositories;
+import com.portal_egressos.portal_egressos_backend.models.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -6,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import com.portal_egressos.portal_egressos_backend.models.Usuario;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -15,19 +19,98 @@ public class UsuarioRepositoryTest {
     UsuarioRepository repository;
 
     @Test
-    public void deveVerificarSalvarUsuario() {
+    public void deveSalvarUsuario() {
         //cenário
         Usuario user = Usuario.builder().email("teste@teste.com")
-                                        .senha("123").build();
+                                        .senha("123")
+                                        .build();
 
         //ação
-        Usuario salvo = repository.save(user);
+        Usuario saved = repository.save(user);
         //verificação
-        Assertions.assertNotNull(salvo);
-        Assertions.assertEquals(user.getEmail(), salvo.getEmail());
-        Assertions.assertEquals(user.getSenha(), salvo.getSenha());
+        Assertions.assertNotNull(saved);
+        Assertions.assertEquals(user.getEmail(), saved.getEmail());
+        Assertions.assertEquals(user.getSenha(), saved.getSenha());
+    }
 
-        
+    
+    @Test
+    public void deveSalvarVariosUsuarios() {
+        // cenário
+        List<Usuario> users = new ArrayList<Usuario>();
+        for (int i = 0; i < 3; i++) {
+            users.add(
+                    Usuario.builder().email("teste@teste.com")
+                                        .senha("123")
+                                        .build());
+        }
+
+        // ação
+        List<Usuario> saved = repository.saveAll(users);
+        repository.deleteAll(saved);
+
+        // verificação
+        Assertions.assertNotNull(saved);
+        Assertions.assertEquals(users.size(), saved.size());
+        for (int i = 0; i < users.size(); i++) {
+            Assertions.assertEquals(users.get(i).getEmail(), saved.get(i).getEmail());
+        }
+    }
+
+
+    @Test
+    public void deveAtualizarUsuario() {
+        // cenário
+        Usuario user = Usuario.builder().email("teste@teste.com")
+                                        .senha("123")
+                                        .build();
+
+        // ação
+        Usuario saved = repository.save(user);
+        saved.setEmail("teste2@teste.com");
+        saved.setSenha("testesenha");
+        Usuario returned = repository.save(saved);
+
+        // verificação
+        Assertions.assertNotNull(returned);
+        Assertions.assertEquals(saved.getId(), returned.getId());
+        Assertions.assertEquals(saved.getEmail(), returned.getEmail());
+        Assertions.assertEquals(saved.getSenha(), returned.getSenha());
+    }
+
+
+    @Test
+    public void deveRemoverUsuario() {
+        // cenário
+        Usuario user = Usuario.builder().email("teste@teste.com")
+                                        .senha("123")
+                                        .build();
+
+        // ação
+        Usuario saved = repository.save(user);
+        Long id = saved.getId();
+        repository.deleteById(id);
+        Optional<Usuario> temp = repository.findById(id);
+
+        // verificação
+        Assertions.assertFalse(temp.isPresent());
+    }
+
+
+    @Test
+    public void deveObterEgressoPorEmail() {
+        // cenário
+        Usuario user = Usuario.builder().email("teste@teste.com")
+                                        .senha("123")
+                                        .build();
+
+        // ação
+        repository.save(user);
+        Optional<Usuario> returned = repository.findByEmail(user.getEmail());
+
+        // verificação
+        Assertions.assertTrue(returned.isPresent());
+        Assertions.assertEquals(user.getEmail(), returned.get().getEmail());
     }
 }
 
