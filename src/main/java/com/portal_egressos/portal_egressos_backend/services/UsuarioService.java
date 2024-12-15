da8c1a19362c1bd6c07e75c0b6be8906f5b351d7
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2024-12-14 23:17:38 -0300

    feat: add usuario service

--- src/main/java/com/portal_egressos/portal_egressos_backend/services/UsuarioService.java
+++ src/main/java/com/portal_egressos/portal_egressos_backend/services/UsuarioService.java
@@ -1,5 +1,30 @@
 package com.portal_egressos.portal_egressos_backend.services;
 
-public class UsuarioService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import com.portal_egressos.portal_egressos_backend.exceptions.RegraNegocioRunTime;
+import com.portal_egressos.portal_egressos_backend.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService implements UserDetailsService{
+    
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String login) {
+        UserDetails user = usuarioRepository.findByEmail(login);
+
+        if (user == null) {
+            throw new RegraNegocioRunTime("Conta não encontrada");
+        }
+        
+        return user; 
+
+    }
+
 
 }
