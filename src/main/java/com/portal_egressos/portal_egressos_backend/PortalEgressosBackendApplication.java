e356a23e33a78cd15469f9b01867fd2ece71e084
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2024-12-14 22:16:14 -0300

    feat: add PropertySource to get .env in main class

--- src/main/java/com/portal_egressos/portal_egressos_backend/PortalEgressosBackendApplication.java
+++ src/main/java/com/portal_egressos/portal_egressos_backend/PortalEgressosBackendApplication.java
@@ -2,8 +2,10 @@ package com.portal_egressos.portal_egressos_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:${user.dir}/.env")
 public class PortalEgressosBackendApplication {
 
 	public static void main(String[] args) {
